082e8e092a7e35423ad1f147dbf2e40a86cd9b82
Arthur Vilmar Rita <arthur1590@hotmail.com>
2020-01-24 22:11:03 -0300

    First Commit
    Creating project structure.

---
+++ FutebaProfissional.Repositories/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FutebaProfissional.Repositories
+{
+    public class Class1
+    {
+    }
+}
---
+++ FutebaProfissional.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FutebaProfissional.Services
+{
+    public class Class1
+    {
+    }
+}
