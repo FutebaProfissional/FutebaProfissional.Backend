8c079d75ebfd84f287b9e2774aadda3ab9b05ca4
Arthur Vilmar Rita <arthur1590@hotmail.com>
2020-02-01 12:47:22 -0300

    Refactoring initialization

--- FutebaProfissiona.Backend/FutebaProfissiona.Api/Startup.cs
+++ FutebaProfissiona.Backend/FutebaProfissiona.Api/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using FutebaProfissional.Domain.Profiles;
 using FutebaProfissional.Repositories.Abstractions;
@@ -9,14 +5,12 @@ using FutebaProfissional.Repositories.Context;
 using FutebaProfissional.Repositories.Repositories;
 using FutebaProfissional.Services;
 using FutebaProfissional.Services.Abstractions;
+using FutebaProfissional.Services.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace FutebaProfissiona.Api
 {
@@ -32,14 +26,15 @@ namespace FutebaProfissiona.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IGroupService, GroupService>();
-            services.AddScoped<IGroupRepository, GroupRepository>();
             services.AddControllers();
-            services.AddControllersWithViews();
-            services.AddAutoMapper(typeof(Startup));
             services.AddDbContext<FutebaDbContext>();
-            //services.AddScoped<IGroupService, GroupService>();
+            RegisterIoC.Register(services);
+            ConfigureAutoMapper(services);
+        }
 
+        private static void ConfigureAutoMapper(IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(Startup));
             var mappingConfig = new MapperConfiguration(mc =>
             {
                 mc.AddProfile(new GroupProfile());
@@ -47,7 +42,6 @@ namespace FutebaProfissiona.Api
 
             IMapper mapper = mappingConfig.CreateMapper();
             services.AddSingleton(mapper);
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ FutebaProfissiona.Backend/FutebaProfissional.Services/IoC/RegisterIoC.cs
@@ -0,0 +1,19 @@
+﻿using FutebaProfissional.Repositories.Abstractions;
+using FutebaProfissional.Repositories.Repositories;
+using FutebaProfissional.Services.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FutebaProfissional.Services.IoC
+{
+    public class RegisterIoC
+    {
+        public static void Register(IServiceCollection services)
+        {
+            services.AddScoped<IGroupService, GroupService>();
+            services.AddScoped<IGroupRepository, GroupRepository>();
+        }
+    }
+}
